913bbdc244ec28c5d67c5ddbbd9446a7be080de1
Nicolas Perez <nico16perez@gmail.com>
2025-10-09 19:04:00 -0300

    Modificar materia

--- src/vistas/VerMaterias.java
+++ src/vistas/VerMaterias.java
@@ -35,11 +35,11 @@ public class VerMaterias extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 409, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 284, Short.MAX_VALUE)
         );
 
         pack();
